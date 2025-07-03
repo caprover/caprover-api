3f4085410c73b8e5224dfea3d3658d5646cd7354
Kasra Bigdeli <kasra85@gmail.com>
2025-07-02 22:56:05 -0700

    Removed comment

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,6 @@ Promise.resolve()
         console.log(error)
     })
 
-// Think about what if one future requests fails and we need to reuse the same password and otp
-// For example, if the server restarts, we need to re-authenticate the user using the same password
-// and otp. So we need to store them somewhere. Maybe in the ApiManager instance itself?
-
 // TODO: a good example of how to export with typescript type information
 // https://github.com/dgreif/ring/blob/main/packages/ring-client-api/package.json
 //
